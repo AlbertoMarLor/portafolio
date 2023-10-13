fdf23e7c4322b9a4687aa1df28067116bc964594
AlbertoMarLor <amartinl92@gmail.com>
2023-10-13 12:37:42 +0200

    correcion del footer en el responsive

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <footer className='footer'>Portafolio hecho con React - 2023 Alberto Martín Lorencés</footer>
+        <footer className='footer'>
+            <div className='foot'>
+                <p>Portafolio hecho con React - 2023&nbsp;</p>
+                <p>Alberto Martín Lorencés</p>
+            </div>
+        </footer>
     )
 }
